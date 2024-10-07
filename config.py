ec6c4eccf8c30c6950322902432ee34d98891f5a
JevnJyoti <87808469+JevnJyoti@users.noreply.github.com>
2024-10-08 01:23:27 +0530

    config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6865080558:AAG78HHqTGVoBkX1A4UWYnvUCJGfTZ0Hc")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7653442779:AAE2-pbcr0gBdivQ5s-gvnw-xyTTyuEYXhI")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "22451323"))
+API_ID = int(os.environ.get("API_ID", "24105377"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "4783925ba83751dfadedcc961bf1275")
+API_HASH = os.environ.get("API_HASH", "4e105c49f861714b5e6cb2b0b5dc87ef")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://heroku:heroku@cluster0.wamwxpr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://selolo4203:DejoqHYfqjTui1VD@cluster0.3uppl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
